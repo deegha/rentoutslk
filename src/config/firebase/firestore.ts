bfbe0b1130c258966c5f58fbdd6c0ea0cf9207c8
Mykhail <115717017+mykhail-druz@users.noreply.github.com>
2024-06-13 23:46:32 +0300

    For backup

--- src/config/firebase/firestore.ts
+++ src/config/firebase/firestore.ts
@@ -5,7 +5,11 @@ const firebaseAdminConfig = {
   credential: cert({
     projectId: process.env.AUTH_FIREBASE_PROJECT_ID,
     clientEmail: process.env.AUTH_FIREBASE_CLIENT_EMAIL,
+<<<<<<< HEAD
     privateKey: process.env.AUTH_FIREBASE_PRIVATE_KEY?.replace(/\\n/g, '\n'),
+=======
+    privateKey: process.env.AUTH_FIREBASE_PRIVATE_KEY?.replace(/\\n/g, '\n'), // Handle newline characters
+>>>>>>> 6428547 (For backup)
   }),
 };
 
