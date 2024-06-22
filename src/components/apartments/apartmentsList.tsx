b210fff416ddfa0454c02ee958ec0e6c31e82ffe
Михаил <115717017+mykhail-druz@users.noreply.github.com>
2024-06-22 17:37:09 +0300

    add apartments section on home page

--- src/components/apartments/apartmentsList.tsx
+++ src/components/apartments/apartmentsList.tsx
@@ -1,5 +1,20 @@
 import React from 'react';
 
+import styles from './apartmentsList.module.scss';
+
+import { ApartmentsCard } from './apartmentsCard';
+
 export const ApartmentsList = () => {
-  return <div>Apartments List</div>;
+  return (
+    <div className={styles.list}>
+      <ApartmentsCard />
+      <ApartmentsCard />
+      <ApartmentsCard />
+      <ApartmentsCard />
+      <ApartmentsCard />
+      <ApartmentsCard />
+      <ApartmentsCard />
+      <ApartmentsCard />
+    </div>
+  );
 };
--- src/components/apartmentsHome/apartmentsHome.tsx
+++ src/components/apartmentsHome/apartmentsHome.tsx
@@ -1,16 +1,31 @@
 import React from 'react';
+
+import styles from './apartmentsHome.module.scss';
+
 import { ApartmentsList } from '../apartments/apartmentsList';
+import { Button } from '../button';
 
 export const ApartmentsHome = () => {
   return (
-    <section>
-      <div>
-        <h2>Apartments for you</h2>
-        <p>
+    <section className={styles.container}>
+      <div className={styles.titleBlock}>
+        <h2 className={styles.title}>Apartments for you</h2>
+        <p className={styles.subtitle}>
           This properties are tending. Find a place, contact owner, book a tour.
         </p>
       </div>
       <ApartmentsList />
+      <div className={styles.btnBlock}>
+        <Button
+          text="Show more"
+          textColor="#222222"
+          fontWeight="600"
+          padding="14.5px 20px"
+          borderRadius="4px"
+          bgColor="#ffffff"
+          borderColor="#222222"
+        />
+      </div>
     </section>
   );
 };
