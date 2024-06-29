fd80b52e8e355a89d4b51230389743c2028ea2a7
Mykhail <115717017+mykhail-druz@users.noreply.github.com>
2024-06-29 13:54:58 +0300

    Backup

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -2,6 +2,10 @@ import React from 'react';
 import Image from 'next/image';
 import styles from './header.module.scss';
 import Favorite from '@/icons/carbon_favorite.svg';
+import { Button } from '../button';
+import Logo from '@/icons/logo.svg';
+import { auth } from '@/auth';
+import { AuthButton } from '@/components/auth';
 
 export const Header = async () => {
   const session = await auth();
@@ -22,10 +26,24 @@ export const Header = async () => {
           <a className={styles.favorite}>
             <Favorite />
           </a>
-          <a className={styles.logIn}>Log in</a>
-          <a className={styles.signUpBtn}>
-            <p>Sign up</p>
-          </a>
+          <pre>{JSON.stringify(session, null, 2)}</pre>
+
+          <AuthButton className={styles.logIn} />
+          {!session ? (
+            <>
+              <Button
+                text="SIGN UP"
+                link="/"
+                bgColor="#000"
+                textColor="#fff"
+                padding="10px"
+                borderRadius="4px"
+                fontWeight="600"
+              />
+            </>
+          ) : (
+            <></>
+          )}
         </div>
       </nav>
     </header>
