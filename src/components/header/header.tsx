957208a26b64df05966188648f1894e5bec3446d
Danylo Kachanko <d.kachanko@gmail.com>
2024-07-05 23:20:09 +0300

    Add auth system

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -2,6 +2,19 @@ import React from 'react';
 import Image from 'next/image';
 import styles from './header.module.scss';
 import Favorite from '@/icons/carbon_favorite.svg';
+import Logo from '@/icons/logo.svg';
+import { SignModal } from '@/components/auth';
+import { auth } from '@/auth';
+import { Session as AuthSession, User as AuthUser } from '@auth/core/types';
+
+interface User extends AuthUser {
+  id: string;
+  email: string;
+}
+
+interface CustomSession extends AuthSession {
+  user: User;
+}
 
 export const Header = async () => {
   const session = (await auth()) as CustomSession | null;
@@ -21,10 +34,26 @@ export const Header = async () => {
           <a className={styles.favorite}>
             <Favorite />
           </a>
-          <a className={styles.logIn}>Log in</a>
-          <a className={styles.signUpBtn}>
-            <p>Sign up</p>
-          </a>
+          {/* <pre>{JSON.stringify(session, null, 2)}</pre> */}
+
+          {/* <AuthButton className={styles.logIn} /> */}
+          {/* {!session ? (
+            <>
+              <Button
+                text="SIGN UP"
+
+                bgColor="#000"
+                textColor="#fff"
+                padding="10px"
+                borderRadius="4px"
+                fontWeight="600"
+              />
+            </>
+          ) : (
+             
+          )} */}
+
+          <SignModal session={session} />
         </div>
       </nav>
     </header>
