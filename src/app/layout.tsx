5984d196e09ab22352f04fccdb630dc42ca151fe
Михаил <115717017+mykhail-druz@users.noreply.github.com>
2024-10-22 04:11:43 +0300

    Fixed deployment issue

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,8 +3,36 @@ import { Inter } from 'next/font/google';
 import { SessionProvider } from 'next-auth/react';
 import { FavouriteProvider } from '@/context/favouriteProvider/favouriteProvider';
 import { SessionTimeoutProvider } from '@/context/sessionTimeout/SessionTimeoutProvider';
+import type { Metadata } from 'next';
 
 const inter = Inter({ subsets: ['latin'] });
+
+export const metadata: Metadata = {
+  metadataBase: new URL('https://rentoutslk.vercel.app'),
+  title: 'rentoutslk | Innovative Property Management Solutions In Sri Lanka',
+  description:
+    'Discover innovative property management solutions in Sri Lanka with our technology-driven approach.',
+  openGraph: {
+    title: 'RentoutSLK | Innovative Property Management Solutions',
+    description:
+      'Discover innovative property management solutions in Sri Lanka with our technology-driven approach.',
+    url: 'https://rentoutslk.vercel.app',
+    siteName: 'rentoutslk',
+    images: [
+      {
+        url: '/og.png',
+        alt: 'rentoutslk',
+      },
+    ],
+  },
+  twitter: {
+    card: 'summary_large_image',
+    title: 'rentoutslk | Innovative Property Management Solutions',
+    description:
+      'Discover innovative property management solutions in Sri Lanka with our technology-driven approach.',
+    images: ['/og.png'],
+  },
+};
 
 export default function RootLayout({
   children,
