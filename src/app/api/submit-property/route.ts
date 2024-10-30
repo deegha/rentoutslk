b51bb2b84fd323ac9c8b5865a5a6a093102aced8
Михаил <115717017+mykhail-druz@users.noreply.github.com>
2024-10-30 05:08:29 +0200

    Fixed SendGrid emails, auth fallbacks pages and list property responsiveness

--- src/app/api/submit-property/route.ts
+++ src/app/api/submit-property/route.ts
@@ -1,15 +1,16 @@
-// import { NextRequest, NextResponse } from 'next/server';
+import { NextRequest, NextResponse } from 'next/server';
 
-// export async function POST(req: NextRequest) {
-//   try {
-//     const _data = await req.json();
+export async function POST(req: NextRequest) {
+  try {
+    const data = await req.json();
+    console.log('Received form data:', data);
 
-//     return NextResponse.json({ message: 'Form submitted successfully' });
-//   } catch (error) {
-//     console.error('Form submission error:', error);
-//     return NextResponse.json(
-//       { message: 'Internal server error' },
-//       { status: 500 },
-//     );
-//   }
-// }
+    return NextResponse.json({ message: 'Form submitted successfully' });
+  } catch (error) {
+    console.error('Form submission error:', error);
+    return NextResponse.json(
+      { message: 'Internal server error' },
+      { status: 500 },
+    );
+  }
+}
